1417dbd472af80d50f6a91b0bcf25e26b88f3e11
student <student@DEV2-15>
2017-12-07 12:30:48 +0900

    更新

--- Enemy project/Enemy project/ObjEnemy.h
+++ Enemy project/Enemy project/ObjEnemy.h
@@ -6,8 +6,19 @@
 using namespace GameL;
 
 //オブジェクト：敵
-class ObjEnemy :public CObj
+class CObjEnemy :public CObj
 {
 public:
-	ObjEnemy() {};
+	CObjEnemy() {};
+	~CObjEnemy() {};
+	void Init();
+	void Action();
+	void Draw();
+
+private:
+	float m_x;//敵のx方向の位置
+	float m_y;//敵のy方向の位置
+	float m_vx;//敵の移動ベクトルX
+	float m_vy;//敵の移動ベクトルY
+	
 };
